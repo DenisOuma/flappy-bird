100f8bb31df7a0e89fc0932e68a2642d229e7a20
DenisOuma <dennisouma38@gmail.com>
2024-04-16 08:42:36 +0300

    Horizontal distance pipe

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const config = {
 		// setting up the physics engine to be used in the game
 		default: "arcade",
 		arcade: {
-			gravity: { y: 400 },
+			// gravity: { y: 400 },
 			debug: true,
 		},
 	},
@@ -22,17 +22,26 @@ const config = {
 	},
 };
 
+const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
+
+let bird = null;
+let pipes = null;
+
+let pipeHorizontalDistance = 0;
+
+// The Pippe Distance Range
+const pipeVerticalDistanceRange = [150, 250];
+
+const flapVelocity = 250;
+let totalDelta = null;
+const initialBirdPosion = { x: config.width / 15, y: config.height / 2 };
 // // preload function to load the assets before the game starts
 function preload() {
 	this.load.image("sky", "assets/sky.png");
 	this.load.image("bird", "assets/bird.png");
+	this.load.image("pipe", "assets/pipe.png");
 }
-const VELOCITY = 200;
-
-let bird = null;
-const flapVelocity = 250;
-let totalDelta = null;
-const initialBirdPosion = { x: config.width / 15, y: config.height / 2 };
 
 function create() {
 	// Use setOrigin to set the origin of the image to the top left corner
@@ -40,6 +49,19 @@ function create() {
 	bird = this.physics.add
 		.sprite(initialBirdPosion.x, initialBirdPosion.y, "bird")
 		.setOrigin(0);
+	bird.body.gravity.y = 400;
+
+	pipes = this.physics.add.group();
+
+	// Creating the pipes
+	for (let i = 0; i < PIPES_TO_RENDER; i++) {
+		const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+		const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
+
+		placePipe(upperPipe, lowerPipe);
+	}
+
+	pipes.setVelocityX(-VELOCITY);
 
 	this.input.on("pointerdown", flap);
 	this.input.keyboard.on("keydown-SPACE", flap);
@@ -51,16 +73,37 @@ function update(time, delta) {
 	}
 }
 
-// Flapping Function for the bird
-const flap = () => {
-	bird.body.velocity.y = -flapVelocity;
-	console.log("flap", bird.body.velocity.y);
+// Pipe Placement Function
+const placePipe = (uPipe, lPipe) => {
+	pipeHorizontalDistance += 400;
+	pipeHorizontalDistance = getRightMostPipe();
+	let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+	let pipeverticalPosition = Phaser.Math.Between(
+		0 + 20,
+		config.height - 20 - pipeVerticalDistance
+	);
+
+	uPipe.x = pipeHorizontalDistance;
+	uPipe.y = pipeverticalPosition;
+
+	lPipe.x = uPipe.x;
+	lPipe.y = uPipe.y + pipeVerticalDistance;
 };
 
-const gameOver = () => {};
+// GetMostRightPipe
 
+const getRightMostPipe = () => {};
+
+// Flapping Function for the bird
 const restartPlayerPosition = () => {
 	bird.x = initialBirdPosion.x;
 	bird.y = initialBirdPosion.y;
 };
+const flap = () => {
+	bird.body.velocity.y = -flapVelocity;
+	// console.log("flap", bird.body.velocity.y);
+};
+
+const gameOver = () => {};
+
 new Phaser.Game(config);
